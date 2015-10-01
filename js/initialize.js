f89de8d15dcf76474b82a81d4263ebbabb15b3c9
MrCodeCollector <alexaaronpena@gmail.com>
2015-10-01 16:56:51 -0700

    alhs sj website redesign
    Redesign for the Abraham Lincoln High School San Jose Webpage.

---
+++ js/initialize.js
@@ -0,0 +1,10 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('.collapsible').collapsible();
+    $('.slider').slider({full_width: true});
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
