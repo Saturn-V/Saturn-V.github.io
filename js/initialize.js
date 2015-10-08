bffcb40e35426caf2334e15e4404765bcbd82b7f
MrCodeCollector <alexaaronpena@gmail.com>
2015-10-08 00:37:08 -0700

    Update
    Update files

--- js/initialize.js
+++ js/initialize.js
@@ -5,6 +5,8 @@
     $('.parallax').parallax();
     $('.collapsible').collapsible();
     $('.slider').slider({full_width: true});
+    $('ul.tabs').tabs();
+
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
