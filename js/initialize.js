af3f5849a1801d91b9f50dffe83eaf6b0e49e99f
MrCodeCollector <alexaaronpena@gmail.com>
2015-10-27 17:25:22 -0700

    Last minute Update
    Reverted HackUMAss III files back to LHS Website files for school
    presentation.

---
+++ js/initialize.js
@@ -0,0 +1,12 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('.collapsible').collapsible();
+    $('.slider').slider({full_width: true});
+    $('ul.tabs').tabs();
+
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
