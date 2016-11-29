1b36368166af5ef14d2c4bdf12ff4fa335cffd8a
Alex Aaron Pena <alexaaronpena@gmail.com>
2016-11-28 20:18:09 -0800

    Refactored code, updated UI to final

--- js/index.js
+++ js/index.js
@@ -6,3 +6,19 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+
+// Smooth Scrolling with jquery
+$(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 500);
+                return false;
+            }
+        }
+    });
+});
